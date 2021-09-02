62d5d52c59014c1764c5d469bbacbeaca55d16ae
Sanchi2011 <87849808+Sanchi2011@users.noreply.github.com>
2021-09-02 09:55:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+img = "";
+
+function setup(){
+    canvas = createCanvas(640,420)
+    canvas.center();
+    
+}
+
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#000000");
+    text("Dog",45,75);
+    noFill();
+    stroke("#00ffff");
+    rect(30,60,450,350);
+}
